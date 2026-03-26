a2aa14783d0f88cdfa1b13a3a39b85de9f15b8f6
Bishaw <bishawmekitachew@gmail.com>
2026-03-26 16:47:36 +0300

    restarting the things

---
+++ try.session.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS school_db;
+USE school_db;
+CREATE TABLE IF NOT EXISTS students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(50)
+);
+
